02737b01658cce021feb9fb03b5ad3b78acdeb35
Pedro Reis <143048029+PHDReis@users.noreply.github.com>
2025-03-13 10:36:34 -0300

    Update aula05_desafio.05.4.py

--- Aula05/aula05_desafio.05.4.py
+++ Aula05/aula05_desafio.05.4.py
@@ -1,14 +1,18 @@
 print("Nuúmero mágico, você terá 3 tentativas para acertar um número entre 1 e 10")
-numero=int(input("Tentativa 1, digite o número: "))
+numero1=int(input("Tentativa 1, digite o número: "))
+numero2=int(input("Tentativa 2, digite o número: "))
+numero3=int(input("Tentativa 3, digite o número: "))
 
 numero_magico=5
 
-if (numero==numero_magico):
-    print("Parabens, você acertou!")
-elif numero=int(input("Você errou, tentativa 2, digite o numero: ")):
-    (numero==numero_magico)
-    print("Parabens, você acertou!")
-elif numero=int(input("Você errou, tentativa 3, digite o numero: ")):
-    (numero==numero_magico)
-    print("Parabens, você acertou!")
-else:("Não foi dessa vez :(")
+if (numero1==numero_magico):
+    print(f"Parabens, você acertou! O número mágico é {numero1} da 1ª tentativa")
+
+elif (numero2==numero_magico):
+    print(f"Parabens, você acertou! O número mágico é {numero2} da 2ª tentativa")
+
+elif (numero3==numero_magico):
+    print(f"Parabens, você acertou! O número mágico era {numero3} da 3ª tentativa")
+
+else:
+    print("Não foi dessa vez :(")
