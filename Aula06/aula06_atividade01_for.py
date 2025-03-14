efb800231a0beea342d5be707b32d275149ae531
Pedro Reis <143048029+PHDReis@users.noreply.github.com>
2025-03-14 20:56:55 -0300

    Update aula06_atividade01_for.py

--- Aula06/aula06_atividade01_for.py
+++ Aula06/aula06_atividade01_for.py
@@ -1,4 +1,11 @@
 frutas=["Maça","Banana","Cereja"]
 
 for fruta in frutas:
-    print(fruta)
+    print (f"\t{fruta.upper()}")
+    input("\n Pressione Enter\n")
+
+
+
+contagem = (9,8,7,6,5,4,3,2,1,0)
+for item in contagem:
+    print(f"{item}...")
