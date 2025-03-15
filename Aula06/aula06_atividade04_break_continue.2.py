9f17ed7115dbe37efd24d047248e8d7eda78d496
Pedro Reis <143048029+PHDReis@users.noreply.github.com>
2025-03-14 21:39:24 -0300

    Add files via upload

---
+++ Aula06/aula06_atividade04_break_continue.2.py
@@ -0,0 +1,9 @@
+for i in range(100):
+
+    if i == 52:
+        break 
+
+    if i % 2 == 1: # if i % 2 == 0: imprime os Ímpares
+        continue 
+
+    print(i)
---
+++ Aula06/aula06_atividade04_break_continue.py
@@ -0,0 +1,9 @@
+for i in range(10):
+
+    if i == 5:
+        break #Para o loop quando i for 5
+
+    if i % 2 == 0:
+        continue #Pule número pares
+
+    print(i)
