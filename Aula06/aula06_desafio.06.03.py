50c58cbf1a28b5c39a43b2532833865b0fe52a72
Pedro Reis <143048029+PHDReis@users.noreply.github.com>
2025-03-19 19:47:56 -0300

    Add files via upload

---
+++ Aula06/aula06_desafio.06.03.py
@@ -0,0 +1,9 @@
+for numero in range (1,21):
+
+    if numero % 2 == 0:
+        print(f"Os números pares são: {numero}")
+    
+for numero in range (1,21):
+
+    if numero % 2 != 0:
+        print(f"Os números ímpares são: {numero}")
